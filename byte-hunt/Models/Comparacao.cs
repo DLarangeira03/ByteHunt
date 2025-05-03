430fba28aab4a4184004a938ab8be8c0a31133ca
DLarangeira <diogo.f.larangeira@gmail.com>
2025-05-03 10:05:43 +0100

    Criação dos primeiros modelos e definição dos atributos dos mesmos.

---
+++ byte-hunt/Models/Comparacao.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace byte_hunt.Models;
+
+public class Comparacao
+{
+    //Atributos da classe
+    [Key]
+    public int Id { get; set; } //identificador
+    
+    [Required]
+    public DateTime Data { get; set; } //Data da comparação
+    
+    [Required]
+    public int UtilizadorId { get; set; }// Chave estrangeira
+    
+    [ForeignKey("UtilizadorId")]
+    public Utilizador Utilizador { get; set; } //utilizador que realiza a comparação
+    
+    public ICollection<Item> Itens { get; set; } = new List<Item>();//Relação com os itens comparados
+}
---
+++ byte-hunt/Models/Item.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace byte_hunt.Models;
+
+public class Item
+{ 
+    //Atributos da classe
+    [Key]
+    public int Id { get; set; } //identificador
+    
+    [Required]
+    public string Nome { get; set; } //nome do item
+    
+    
+    public string Marca { get; set; } //marca do item
+    
+    [Required]
+    public string Descricao { get; set; } //descrição do item
+    
+    public decimal Preco { get; set; } //preço do item
+    
+    [Required]
+    public int CategoriaId { get; set; } //chave estrangeira
+    
+    [ForeignKey("CategoriaId")]
+    public Categoria Categoria { get; set; } //categoria do item
+    
+    public ICollection<Comparacao> Comparacoes { get; set; } = new List<Comparacao>(); //Relação muitos-para-muitos com Comparacao   
+
+        
+    
+}
