6b6ef6cd51fc5314ef95f94fffebffd5c461ab37
Benquerer <andrebenquerer@gmail.com>
2025-05-21 12:40:34 +0100

    Migration fix

--- byte-hunt/Migrations/20250521113813_NewModelAttributes.Designer.cs
+++ byte-hunt/Migrations/20250521113813_NewModelAttributes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using byte_hunt.Data;
 
@@ -11,9 +12,11 @@ using byte_hunt.Data;
 namespace byte_hunt.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250521113813_NewModelAttributes")]
+    partial class NewModelAttributes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
