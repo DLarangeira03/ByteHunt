28d6597ef15e2f954af62cbb173a7ebf787f3d92
DLarangeira <larangeiratrabalhos@gmail.com>
2025-05-22 12:29:32 +0100

    Visual inicial da página home

--- byte-hunt/Controllers/ItensController.cs
+++ byte-hunt/Controllers/ItensController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using byte_hunt.Data;
 using byte_hunt.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace byte_hunt.Controllers
 {
