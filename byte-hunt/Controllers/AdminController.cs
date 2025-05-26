52296fe4df04f9a1149d6d0703fa0bc3c8ad599c
Benquerer <andrebenquerer@gmail.com>
2025-05-26 03:06:09 +0100

    Added "Info" and "Confirmation" popups. Confirmation still need to be fixed. NUllREFERENCE somehow

--- byte-hunt/Controllers/AdminController.cs
+++ byte-hunt/Controllers/AdminController.cs
@@ -65,6 +65,7 @@ public class AdminController : Controller {
         if (user != null && await _roleManager.RoleExistsAsync(role)) {
             await _userManager.RemoveFromRoleAsync(user, role);
         }
+        TempData["RoleRemovalConfirmation"] = $"Permissão \"{role}\" removida com sucesso do utilizador \"{user.UserName}\"";
         return RedirectToAction("ManageRoles");
     }
     
---
+++ byte-hunt/Models/Popups/ConfirmPopupViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace byte_hunt.Models.Popups;
+
+public class ConfirmPopupViewModel : PopupViewModel {
+    
+    public string Pergunta { get; set; } = "Tem certeza que deseja continuar?";
+    public string ConfirmBtnText { get; set; } = "Sim";
+    public string CancelBtnText { get; set; } = "Não";
+    public string ActionUrl { get; set; }
+    public string FormMethod { get; set; } = "POST";
+
+}
