a7779c0036e073fe98986de00596edce1c5388ba
DLarangeira <larangeiratrabalhos@gmail.com>
2025-06-16 22:38:41 +0100

    Adição do JWT e Swagger.
    Já é possível aceder ao swagger onde conseguimos ver todas os endpoints.
    O JWT já está funcional e já conseguimos gerar tokens

--- byte-hunt/Program.cs
+++ byte-hunt/Program.cs
@@ -3,17 +3,24 @@ using Microsoft.EntityFrameworkCore;
 using System.Globalization;
 using byte_hunt.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 // metodo para definir roles
 // isso garante que as roles existam na base de dados
-async Task SeedRoles(IServiceProvider serviceProvider) {
+async Task SeedRoles(IServiceProvider serviceProvider)
+{
     var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-    string[] roleNames = {"User", "Moderator", "Administrator"};
-    foreach (var roleName in roleNames) {
-        if(!await roleManager.RoleExistsAsync(roleName)) {
+    string[] roleNames = { "User", "Moderator", "Administrator" };
+    foreach (var roleName in roleNames)
+    {
+        if (!await roleManager.RoleExistsAsync(roleName))
+        {
             await roleManager.CreateAsync(new IdentityRole(roleName));
         }
     }
@@ -35,18 +42,72 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
         sqlOptions => sqlOptions.EnableRetryOnFailure()
     ));
 
-builder.Services.AddDefaultIdentity<Utilizador>(options => {
+builder.Services.AddDefaultIdentity<Utilizador>(options =>
+{
     options.SignIn.RequireConfirmedAccount = true; // email deve ser confirmado para login
-    options.Password.RequireUppercase = false; 
-    options.Password.RequireNonAlphanumeric = false; 
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
 }).AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddRazorPages();
 
+var jwtKey = builder.Configuration["JwtSettings:SecretKey"];
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false, // ajustar conforme necessário
+            ValidateAudience = false, // ajustar conforme necessário
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+        };
+    });
+
+// Configurar controllers e Swagger
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    // Configurar Swagger para JWT
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"Autenticação JWT. 
+                      Insira o token JWT obtido após o login com o prefixo 'Bearer '.
+                      Exemplo: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment()) {
+if (!app.Environment.IsDevelopment())
+{
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
@@ -66,10 +127,19 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-using (var scope = app.Services.CreateScope()) {
+using (var scope = app.Services.CreateScope())
+{
     var services = scope.ServiceProvider;
     await SeedRoles(services);
 }
+
+//jwt Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 
 // run the app
 app.Run();
