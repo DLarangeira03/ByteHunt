7e33e40d6591fb929068a01064c0265f5906f35c
DLarangeira <larangeiratrabalhos@gmail.com>
2025-07-12 16:14:27 +0100

    Revert "Save before pull"
    This reverts commit e77faa3c70b2c0f69d3a0cc2af4766217517ee91.

--- byte-hunt/Migrations/20250627162104_publishPrep.cs
+++ byte-hunt/Migrations/20250627162104_publishPrep.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace byte_hunt.Migrations
 {
     /// <inheritdoc />
-    public partial class Initial : Migration
+    public partial class publishPrep : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
--- byte-hunt/Migrations/ApplicationDbContextModelSnapshot.cs
+++ byte-hunt/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -3,7 +3,6 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using byte_hunt.Data;
 
@@ -12,11 +11,9 @@ using byte_hunt.Data;
 namespace byte_hunt.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250628132804_Initial")]
-    partial class Initial
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
-        /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
